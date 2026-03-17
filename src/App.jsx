30aec3207eaf51ee17d7a042b0e816d0df34f56d
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-03-17 16:30:52 -0300

    adjusted backgorund and added themes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import "./App.css";
+import { Banner } from "./components/Banner";
 import { FormularioDeEvento } from "./components/FormularioDeEvento";
+import { Tema } from "./components/Tema";
 
 // no react, componentes são FUNÇÕES
 
 function App() {
+  const temas = [
+    { id: 1, nome: "front-end" },
+    { id: 2, nome: "back-end" },
+    { id: 3, nome: "devops" },
+    { id: 4, nome: "inteligência artificial" },
+    { id: 5, nome: "data science" },
+    { id: 6, nome: "cloud" },
+  ];
+
+  temas.map;
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="" />
       </header>
-      <section>
-        <img src="/banner.png" alt="" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item}></Tema>
+          </section>
+        );
+      })}
     </main>
   );
 }
